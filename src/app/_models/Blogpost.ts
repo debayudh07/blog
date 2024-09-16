1525f7c410eb2fd3c139a1789563ae341e4f9b4e
deba7 <debayudhbasu@gmail.com>
2024-09-16 17:22:26 +0530

    changes

--- src/app/_models/Blogpost.ts
+++ src/app/_models/Blogpost.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, model, models } from 'mongoose';
+import  { Schema, model, models } from 'mongoose';
 
 const BlogPostSchema = new Schema({
   title: { type: String, required: true },
--- src/app/_models/User.ts
+++ src/app/_models/User.ts
@@ -1,5 +1,5 @@
 // models/User.ts
-import mongoose, { Schema, model, models } from 'mongoose';
+import  { Schema, model, models } from 'mongoose';
 
 // Define the schema
 const UserSchema = new Schema({
