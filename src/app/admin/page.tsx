720e30750b65a82b257d5347c944e72861a702ef
deba7 <debayudhbasu@gmail.com>
2025-09-04 00:55:58 +0530

    v

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -117,7 +117,7 @@ export default function AdminPanel() {
         <div className="relative z-20 flex flex-col min-h-screen">
           <Navbar />
           
-          <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-24">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8 pt-[9rem]">
             <motion.div
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
